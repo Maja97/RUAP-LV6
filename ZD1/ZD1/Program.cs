3c8506f4713aa9691510f96ce07be866059523ef
Maja97 <majaos137@gmail.com>
2020-01-09 19:25:12 +0100

    ZD1 done

--- ZD1/ZD1/Program.cs
+++ ZD1/ZD1/Program.cs
@@ -41,7 +41,7 @@ namespace CallRequestResponseService
                             new StringTable()
                             {
                                 ColumnNames = new string[] {"MPG", "Cyl", "Displacement", "Horsepower", "Weight", "Acceleration", "Year", "CountryCode", "Model"},
-                                Values = new string[,] {  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  }
+                                Values = new string[,] {  { "13", "8", "350", "165", "4274", "12", "72", "1", "chevrolet impala" },  { "18", "8", "307", "130", "3504", "12", "70", "1", "chevrolet chevelle malibu" },  }
                             }
                         },
                     },
